f7c5b43bc656b7deaebf602d1a33f6056f629990
jarrodcolburn <jcourtlandcolburn@gmail.com>
2022-11-19 04:28:06 +0000

    add options

--- src/flutter/install.sh
+++ src/flutter/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
-sudo groupadd --system flutter
-sudo usermod -a -G flutter ${USER}
-sudo mkdir -p ${FLUTTER_ROOT}
-sudo chown $USER:flutter ${FLUTTER_ROOT}
-git clone https://github.com/flutter/flutter.git -b stable ${FLUTTER_ROOT}
-flutter doctor
+git clone \
+    https://github.com/flutter/flutter.git \
+    --branch $CHANNEL \
+    "$FLUTTER_ROOT"
+
+chown \
+    --recursive \
+    $_REMOTE_USER:$_REMOTE_USER \
+    $FLUTTER_ROOT
