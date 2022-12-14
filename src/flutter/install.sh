2fc6724e87c646ed1b7ef1b2cb289edab3437c16
jarrodcolburn <jcourtlandcolburn@gmail.com>
2022-12-14 18:17:24 +0000

    remove cloning fluttter repo

--- src/flutter/install.sh
+++ src/flutter/install.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
 set -e
 
-# git clone \
-#     https://github.com/flutter/flutter.git \
-#     --branch $CHANNEL \
-#     "$FLUTTER_ROOT"
-
-# chown \
-#     --recursive \
-#     $_REMOTE_USER:$_REMOTE_USER \
-#     $FLUTTER_ROOT
-
 FLUTTER_TAR="flutter_linux_${VERSION}-${CHANNEL}.tar.xz" 
 FLUTTER_DOWNLOAD_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FLUTTER_TAR}" 
-FLUTTER_TMP="/tmp/${FLUTTER_TAR}"
-export FLUTTER_HOME="/sdks/flutter" 
-FLUTTER_ROOT="/sdks/flutter" 
-export PATH="${PATH}:${FLUTTER_HOME}/bin"
-wget "${FLUTTER_DOWNLOAD_URL}" -P /tmp \
-    && mkdir -p "/sdks" \
-    && tar -xf "${FLUTTER_TMP}" -C "/sdks" \
-    && rm "${FLUTTER_TMP}" \
-    && git config --global --add safe.directory "${FLUTTER_HOME}" \
-    && flutter doctor
+wget "${FLUTTER_DOWNLOAD_URL}"
+tar -xf "${FLUTTER_TAR}" -C "/usr/local"
+rm "${FLUTTER_TAR}"
+git config --global --add safe.directory "${FLUTTER_HOME}"
+flutter doctor
+chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}"  "${FLUTTER_ROOT}"
