9b4b7059b9e49a9e19eaf1a5c66b2c03dd3fb73f
jarrodcolburn <jcourtlandcolburn@gmail.com>
2022-12-14 18:44:26 +0000

    Install flutter dependencies

--- src/flutter/install.sh
+++ src/flutter/install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Install System Dependencies
+DEBIAN_FRONTEND="noninteractive"
+apt update
+apt -y install --no-install-recommends bash curl file git unzip xz-utils zip libglu1-mesa
+apt clean
+
+# Install Flutter SDK
 FLUTTER_TAR="flutter_linux_${VERSION}-${CHANNEL}.tar.xz" 
 FLUTTER_DOWNLOAD_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FLUTTER_TAR}" 
 wget "${FLUTTER_DOWNLOAD_URL}"
@@ -8,4 +15,4 @@ tar -xf "${FLUTTER_TAR}" -C "/usr/local"
 rm "${FLUTTER_TAR}"
 git config --global --add safe.directory "${FLUTTER_HOME}"
 flutter doctor
-chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}"  "${FLUTTER_ROOT}"
+chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}" "${FLUTTER_ROOT}"
